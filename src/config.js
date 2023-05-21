c0f009416b17e5e337def83233d75e659ec4b6b2
unknown <juanveronelli@gmail.com>
2023-05-21 18:03:38 -0300

    security

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 
 const API_KEY = import.meta.env.VITE_API_KEY
-console.log(API_KEY)
 export const getApiUrl = ({ city }) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 
 export const getForecastUrl = ({ city }) => `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
